432e27a03efcb7514b70216ea91557346bd7757a
adoidadox2 <augusto.geek@gmail.com>
2020-08-06 19:43:49 -0300

    add: User model

---
+++ src/models/User.ts
@@ -0,0 +1,83 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  DeleteDateColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  BeforeInsert,
+  BeforeUpdate,
+  OneToMany,
+} from "typeorm";
+import Ad from "./Ad";
+
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+import authConfig from "../config/authConfig";
+import Purchase from "./Purchase";
+
+@Entity("user")
+export default class User {
+  constructor(name: string, email: string, password: string) {
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
+
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({
+    nullable: false,
+  })
+  name: string;
+
+  @Column({
+    nullable: false,
+  })
+  email: string;
+
+  @Column({
+    nullable: false,
+  })
+  password_hash: string;
+
+  password: string | null;
+
+  @OneToMany((type) => Ad, (ad) => ad.author, {
+    cascade: ["update", "remove", "soft-remove"],
+  })
+  ads: Ad[];
+
+  @OneToMany((type) => Purchase, (purchase) => purchase.user, {
+    cascade: ["update"],
+  })
+  purchases: Purchase[];
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @DeleteDateColumn()
+  deleted_at: Date;
+
+  @BeforeUpdate()
+  @BeforeInsert()
+  async upPass() {
+    this.password_hash = await bcrypt.hash(this.password, 8);
+    this.password = null;
+  }
+
+  checkPassword(password: String) {
+    return bcrypt.compare(password, this.password_hash);
+  }
+
+  generateToken() {
+    return jwt.sign({ id: this.id }, authConfig.secret, {
+      expiresIn: authConfig.ttl,
+    });
+  }
+}
