53d510acfd230411a8f72d2206ea44e1e7b803d2
adoidadox2 <augusto.geek@gmail.com>
2020-08-06 19:44:26 -0300

    add: Ad model

---
+++ src/models/Ad.ts
@@ -0,0 +1,48 @@
+import {
+  PrimaryGeneratedColumn,
+  Entity,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+  ManyToOne,
+  OneToMany,
+} from "typeorm";
+import User from "./User";
+import Purchase from "./Purchase";
+
+@Entity("ad")
+export default class Ad {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({
+    nullable: false,
+  })
+  title: string;
+
+  @Column({
+    nullable: false,
+  })
+  description: string;
+
+  @Column({ type: "real", nullable: false })
+  price: number;
+
+  @ManyToOne((type) => User, (user) => user.ads, {
+    nullable: false,
+  })
+  author: User;
+
+  @OneToMany((type) => Purchase, (purchase) => purchase.ad)
+  purchases: Purchase[];
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @DeleteDateColumn()
+  deleted_at: Date;
+}
