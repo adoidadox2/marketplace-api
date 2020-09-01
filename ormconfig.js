602384cde4c99f7f97e719ba2d7edc3bda3be950
adoidadox2 <augusto.geek@gmail.com>
2020-09-01 16:04:35 -0300

    update .env.example and ormconfig with env variables

---
+++ ormconfig.js
@@ -0,0 +1,14 @@
+module.exports = {
+  type: process.env.DB_TYPE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: ["src/models/**/*.ts"],
+  migrations: ["src/database/migrations/**/*.ts"],
+  cli: {
+    entitiesDir: "src/models",
+    migrationsDir: "src/database/migrations",
+  },
+};
