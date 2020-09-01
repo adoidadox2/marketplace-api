61317522c66047275dd4949b4b8cad56ea52a9a8
adoidadox2 <augusto.geek@gmail.com>
2020-09-01 18:25:13 -0300

    fix environment variable string converted into number

--- src/config/mailConfig.ts
+++ src/config/mailConfig.ts
@@ -1,6 +1,6 @@
 export default {
   host: process.env.MAIL_HOST as string,
-  port: process.env.MAIL_PORT as string,
+  port: parseInt(process.env.MAIL_PORT as string),
   auth: {
     user: process.env.MAIL_USER as string,
     pass: process.env.MAIL_PASS as string,
